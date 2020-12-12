39a863ace62b938f1bbcddf908e9920043562fe9
rusik01 <r.u.s.i.k01@mail.ru>
2020-12-12 15:59:43 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ uglify = require('gulp-uglify'),
 cssmin = require('gulp-cssmin');
 
  gulp.task('sass', function(){
-    return gulp.src('app/scss/style.scss')
+    return gulp.src('app/scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(rename({suffix : '.min'}))
         .pipe(autoprefixer({
@@ -57,7 +57,7 @@ return gulp.src('app/*.html')
     });
 });
  gulp.task('watch', function(){
-gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
 gulp.watch('app/*.html', gulp.parallel('html'))
 gulp.watch('app/js/*.js', gulp.parallel('js'))
  });
